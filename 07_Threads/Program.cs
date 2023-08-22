e6b268d099bba727bb0a17b4662dc7dd12912dc7
Samuel Soares Guimarães <34168199+samuelsoares27@users.noreply.github.com>
2023-08-22 11:54:52 -0300

    Threads

---
+++ 07_Threads/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Threading;
+
+
+
+Thread t1 = new Thread(ThreadRepeticao);
+
+t1.Start();
+
+for (int i = 0; i < 10000; i++)
+{
+    Console.WriteLine($"TESTE TESTE: {i.ToString()}");
+}
+
+
+static void ThreadRepeticao()
+{
+    for(int i = 0; i < 10000; i++)
+    {
+        Console.WriteLine($"Número: {i.ToString()}");
+    }
+}
---
+++ MultiThreads/Program.cs
@@ -0,0 +1,18 @@
+﻿using System.Threading;
+
+for (int i = 0; i < 5; i++)
+{
+    Console.WriteLine($"Thread {i}");
+    Thread t1 = new Thread(ThreadRepeticao);
+
+    t1.Start();
+}
+
+
+static void ThreadRepeticao()
+{
+    for (int i = 0; i < 1000; i++)
+    {
+        Console.WriteLine($"Número: {i.ToString()}");
+    }
+}
