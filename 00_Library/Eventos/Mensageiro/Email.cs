f031ff76706fa86cafd3eeaef784e8f3dce6a9e7
Samuel Soares Guimarães <34168199+samuelsoares27@users.noreply.github.com>
2023-08-23 16:15:20 -0300

    Começando trabalhar com eventos e handlers

---
+++ 00_Library/Eventos/Mensageiro/Email.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _00_Library.Eventos.Mensageiro
+{
+    public class Email
+    {
+        public void EnviarEmail(Video video)
+        {
+            Console.WriteLine($"E-mail enviado para o vídeo {video.Nome}");
+        }
+    }
+}
---
+++ 00_Library/Eventos/VideoEnconde.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _00_Library.Eventos
+{
+    public class VideoEnconde
+    {
+        public delegate void VideoEncodedHandler(Video video);
+        public event VideoEncodedHandler Encoded;
+        public void Encode(Video video){
+            Console.WriteLine("Convertendo o vídeo...");
+            Thread.Sleep(2000);
+            Console.WriteLine("vídeo convertido...");
+
+            Encoded(video);
+        }
+    }
+}
